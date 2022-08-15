53940c386e0f34c784bf579cc895301f3e894d1d
lattesmoody <lattesmoody@gmail.com>
2022-08-15 13:54:41 +0900

    class_12 complete

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,8 +1,14 @@
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
-void main(List<String> arguments) {
-  //print('Hello world: ${dart_application_1.calculate()}!');
-  var hello = 'Hello World';
-  print(hello);
+class Person {
+  String name = 'choco';
+  late int age;
+  late String sex;
+}
+
+void main() {
+  Person p1 = new Person();
+  p1.age = 30;
+  print(p1.age);
 }
